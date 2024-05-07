6b2888940dbcc881cf10198dd3a06f9d14728563
ElvisFMM <115845515+ElvisFMM@users.noreply.github.com>
2024-05-06 23:40:07 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -5,7 +5,7 @@ const santaImage = new Image();
 santaImage.src = "/dragon.png";
 
 const speakerImage = new Image();
-speakerImage.src = "/speaker.png";
+speakerImage.src = "/Arrow.png";
 
 const walkSnow = new Audio("walk-snow.mp3");
 
